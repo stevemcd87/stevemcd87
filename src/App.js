81f05ae21d97a7245b52d4bfafcdcd3f4b3619af
Stephen McDonald <stephenmcdonald8787@gmail.com>
2020-03-18 13:52:15 -0400

    updated src folder

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import Header from "./components/Header/Header";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Technologies from "./components/Technologies/Technologies";
+import NextContainerButton from "./components/NextContainerButton/NextContainerButton";
+
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+// import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+
+Amplify.configure(awsconfig);
 
 function App() {
+  // const [userText, setUserText] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="App">
+      <div id="container-1" className="container">
+        <Header />
+        <NextContainerButton />
+      </div>
+      <div id="container-2" className="container">
+        <Technologies />
+        <NextContainerButton />
+      </div>
+
+      <Portfolio />
     </div>
   );
 }
+
+// <div id="container-2" className="container">
+//   <div>
+//     <h1>About Me</h1>
+//   </div>
+//   <NextContainerButton />
+// </div>
 
 export default App;
